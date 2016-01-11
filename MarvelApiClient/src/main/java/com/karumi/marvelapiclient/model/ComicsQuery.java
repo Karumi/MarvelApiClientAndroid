fb4cb4ddba7da240d31b07d6c6ee0bc46cc72d7e
Jorge J Barroso <jorge@karumi.com>
2016-01-11 15:26:23 +0100

    series api code complete, pending tests

--- MarvelApiClient/src/main/java/com/karumi/marvelapiclient/model/ComicsQuery.java
+++ MarvelApiClient/src/main/java/com/karumi/marvelapiclient/model/ComicsQuery.java
@@ -226,26 +226,6 @@ public class ComicsQuery {
     return returnValues;
   }
 
-  public enum Format {
-    COMIC("comic"),
-    MAGAZINE("magazine"),
-    TRADE_PAPERBACK("trade paperback"),
-    HARDCOVER("hardcover"),
-    DIGEST("digest"),
-    GRAPHIC_NOVEL("graphic novel"),
-    DIGITAL_COMIC("digital comic"),
-    INFINITE_COMIC("infinite comic");
-
-    private final String format;
-
-    private Format(final String format) {
-      this.format = format;
-    }
-
-    @Override public String toString() {
-      return format;
-    }
-  }
 
   public enum FormatType {
     COMIC("comic"),
